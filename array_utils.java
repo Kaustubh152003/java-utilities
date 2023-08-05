51825bdea74d3b7fe7e36966bb1c164d52639656
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-08-05 18:26:29 +0530

    Create array_utils.java

---
+++ array_utils.java
@@ -0,0 +1,37 @@
+public static void print_array(int arr[],int n)
+{
+    int j=0;
+    while(j<n)
+    {
+        System.out.printf("%d ",arr[j]);
+        j++;
+    }
+}
+public static void scan_array(int arr[],int n)
+{
+    Scanner my_scanner=new Scanner(System.in);
+    int j=0;
+    while(j<n)
+    {
+        arr[j]=my_scanner.nextInt();
+        j++;
+    }
+}
+public static int bs(int arr[],int l,int r,int k)
+{
+    if(l<=r)
+    {
+        int m=(r-l+1)/2;
+        if(arr[m]==k)
+        return m;
+        else
+        {
+            if(k<arr[m])
+            return bs(arr,l,m-1,k);
+            else
+            return bs(arr,m+1,r,k);
+        }
+    }
+    else
+    return -1;
+}
